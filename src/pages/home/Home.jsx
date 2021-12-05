2a0f30a7f315f3be6debd15bd8933ca285fe46fa
Gulasal <asal.kuchkarova@gmail.com>
2021-12-05 16:10:11 +0100

    image fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import "./home.css"
 import Header from "../../header/Header"
+import Sidebar from "../../sidebar/Sidebar"
+import Posts from "../../posts/Posts"
 
 export default function Home() {
     return (
-        <div className="home">
+        <>
+            
             <Header />
-            homepage
+            <div className="home">
+                <Sidebar />
+                <Posts />
+           
+            </div>
 
-        </div>
+        </>
     )
 }
