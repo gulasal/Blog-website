053988905fb250bba09f1776bd5f5be88b7e4348
Gulasal <asal.kuchkarova@gmail.com>
2021-12-09 15:08:27 +0100

    login and register pages created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
 
 
 
@@ -10,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <TopBar />
-      <Settings />
+      <Register />
     
     </div>
   );
---
+++ src/components/pages/register/Register.js
@@ -0,0 +1,19 @@
+import "./register.css"
+
+export default function Register() {
+    return (
+        <div className="register">
+            <span className="registerTitle">Register</span>
+            <form className="registerForm">
+                <label>Username</label>
+                <input type="text" className="registerInput" placeholder="Enter your username..."></input>
+                <label>Email</label>
+                <input type="text" className="registerInput" placeholder="Enter your email..."></input>
+                <label>Password</label>
+                <input type="password" className="registerInput" placeholder="Enter your password..."></input>
+                <button className="registerButton">Register</button>
+            </form>
+            <button className="registerLoginButton">Log In</button>
+        </div>
+    )
+}
